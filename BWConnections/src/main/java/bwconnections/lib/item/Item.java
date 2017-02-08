b777fe92148a78fb1fcd9335f99b2c522419598f
Adrian Skrobek <adrian.skrobek@alan-systems.com>
2017-02-08 18:43:57 +0100

    add database and item translator for items

--- BWConnections/src/main/java/bwconnections/lib/item/Item.java
+++ BWConnections/src/main/java/bwconnections/lib/item/Item.java
@@ -1,4 +1,4 @@
-package lib;
+package bwconnections.lib.item;
 
 public class Item {
 
---
+++ BWConnections/src/main/java/bwconnections/lib/translators/ItemTranslator.java
@@ -0,0 +1,11 @@
+package bwconnections.lib.translators;
+
+import bwconnections.lib.item.Item;
+
+public class ItemTranslator {
+	
+	public static Item translateFromDisplayString(String st){
+		return null;
+	}
+
+}
